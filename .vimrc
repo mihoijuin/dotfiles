31330811d9042b21cefdd6fb21a9ec0561cea4ea
Miho Ijuin <miiiiiiiii747@gmail.com>
2019-06-27 20:32:47 +0900

    ClickUp #w9b6n シンボリックリンク生成スクリプト修正、Brewfile修正、vim設定ファイル、karabiner設定ファイル追加

---
+++ .vimrc
@@ -0,0 +1,17 @@
+" カラー
+syntax enable 
+colorscheme molokai
+
+" 入力モードでのカーソル移動
+inoremap <C-q> <Up>
+inoremap <C-n> <Down>
+inoremap <C-b> <Left>
+inoremap <C-f> <Right>
+
+" キー入れ替え
+inoremap <silent> jj <ESC>
+noremap ;w ^
+noremap ;p $
+
+" 行数
+set number
--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DOT_FILES=(.bash_profile)
-FISH_DIRS=(fish)
+DOT_FILES=(.config .vim .vimrc)
+
 
 # シンボリックリンク
 for file in ${DOT_FILES[@]}
@@ -9,13 +9,7 @@ do
     ln -s $HOME/.ghq/github.com/mihoijuin/dotfiles/$file $HOME/$file
 done
 
-for dir in ${FISH_DIRS[@]}
-do
-    ln -s $HOME/.ghq/github.com/mihoijuin/dotfiles/$dir $HOME/.config/$dir
-done
-
-
 # Brewfile
-brew bundle
-brew cleanup
-brew cask cleanup
+#brew bundle
+#brew cleanup
+#brew cask cleanup
